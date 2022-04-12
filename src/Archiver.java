faacb86e55067062f6835d19d1d2934cfcf090e5
Александр <alexwot1212@yandex.ru>
2022-04-12 11:14:08 +0300

    redact encode function

--- src/Archiver.java
+++ src/Archiver.java
@@ -49,7 +49,7 @@ public class Archiver {
     }
 
     private static void encode(File source) throws IOException {     // функция что кодирует исходник
-        List<String> stringList = null;
+        List<String> stringList;
         File inputFile = new File(String.valueOf(source));
         StringBuilder s = new StringBuilder();
         File compressedFile, table;
@@ -58,13 +58,12 @@ public class Archiver {
             stringList = Files.readAllLines(Paths.get(inputFile.getAbsolutePath()));
         } catch (NoSuchFileException e) {
             System.out.println("Неверный путь, или такого файла не существует!");
+            return;
         } catch (MalformedInputException e) {
             System.out.println("Текущая кодировка файла не поддерживается");
-        } catch (IOException e) {
-            e.printStackTrace();
+            return;
         }
 
-        assert stringList != null;
         for (String item : stringList) {
             s.append(item);
             s.append('\n');
@@ -72,11 +71,12 @@ public class Archiver {
 
         HuffmanOperator operator = new HuffmanOperator(new HuffmanTree(s.toString()));
 
-        compressedFile = new File(inputFile.getAbsolutePath() + ".crp");
+        compressedFile = new File(inputFile.getAbsolutePath() + ".cpr");
         compressedFile.createNewFile();
         try (FileOutputHelper fo = new FileOutputHelper(compressedFile)) {
             fo.writeBytes(operator.getByteMsg());
         }
+        //create file with encoding table:
 
         table = new File(inputFile.getAbsolutePath() + ".table.txt");
         table.createNewFile();
@@ -85,10 +85,14 @@ public class Archiver {
         }
 
         System.out.println("Путь к сжатому файлу: " + compressedFile.getAbsolutePath());
+        System.out.println("Путь к кодировочной таблице " + table.getAbsolutePath());
+        System.out.println("Без таблицы файл будет невозможно извлечь!");
 
-        double idealRatio = Math.round(operator.getCompressionRatio() * 100)
-                / (double) 100;                                     //идеализированный коэффициент
+        double idealRatio = Math.round(operator.getCompressionRatio() * 100) / (double) 100;//идеализированный коэффициент
+        double realRatio = Math.round((double) inputFile.length()
+                / ((double) compressedFile.length() + (double) table.length()) * 100) / (double)100;//настоящий коэффициент
 
         System.out.println("Идеализированный коэффициент сжатия равен " + idealRatio);
+        System.out.println("Коэффициент сжатия с учетом кодировочной таблицы " + realRatio);
     }
 }
